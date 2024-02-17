32d82a1e0b64707ee140e556fa9c60e283fa7fe9
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-17 16:41:30 +0300

    Optimised cocktailshaker

--- sorting_algorithms/cocktailshaker.py
+++ sorting_algorithms/cocktailshaker.py
@@ -1,68 +1,48 @@
 #!/usr/bin/env python3
 """Module for cocktailshaker_sort a variation of bubble sort"""
 from sort_speed import Sort_Speed
-from random import randint
-
-
-"""
-procedure cocktailShakerSort(A : list of sortable items) is
-    do
-        swapped := false
-        for each i in 0 to length(A) − 1 do:
-            if A[i] > A[i + 1] then // test whether the two elements are in the wrong order
-                swap(A[i], A[i + 1]) // let the two elements change places
-                swapped := true
-            end if
-        end for
-        if not swapped then
-            // we can exit the outer loop here if no swaps occurred.
-            break do-while loop
-        end if
-        swapped := false
-        for each i in length(A) − 1 to 0 do:
-            if A[i] > A[i + 1] then
-                swap(A[i], A[i + 1])
-                swapped := true
-            end if
-        end for
-    while swapped // if no elements have been swapped, then the list is sorted
-end procedure
-"""
 
 
 def cocktailshaker_sort(array):
+    """
+    A variation of bubble sort that operates bi-directionally
+    """
+
+    top, bot = len(array), 1
+    new_top, new_bot = top, bot
     swapped = True
     while swapped:
         swapped = False
-        print(array)
-        for g in range(1, len(array)):
+        for g in range(bot, top):
             if array[g - 1] > array[g]:
-                array[g] += array[g-1]
-                array[g-1] = array[g] - array[g-1]
-                array[g] = array[g] - array[g-1]
+                b = array[g-1]
+                array[g-1] = array[g]
+                array[g] = b
                 swapped = True
+                new_top = g
+        else:
+            top = new_top
 
         if not swapped:
             break
-        else:
-            swapped = False
 
-        print(array)
-        for h in range(len(array) - 1, 0):
-            if array[h] > array[h + 1]:
-                array[h] += array[h+1]
-                array[h+1] = array[h] - array[h+1]
-                array[h] = array[h] - array[h+1]
+        swapped = False
+        for h in range(top, bot - 1, -1):
+            if array[h] < array[h - 1]:
+                b = array[h-1]
+                array[h-1] = array[h]
+                array[h] = b
                 swapped = True
+                new_bot = h
+        else:
+            bot = new_bot
 
 
 if __name__ == "__main__":
-    """o = Sort_Speed(cocktailshaker_sort)
+    o = Sort_Speed(cocktailshaker_sort)
     print(o.speed_test(), end="\n\n")
 
     print(o.speed_test(1), end="\n\n")
 
     o.arr_len = (5, 50)
-    print(o.speed_test())"""
-
-    cocktailshaker_sort([randint(10, 100) for x in range(15)])
+    print(o.speed_test())
